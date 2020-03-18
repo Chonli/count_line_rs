93b6c6f378bd8b621dff1cfb9081b2c394d68fce
Chonli <Chonli@users.noreply.github.com>
2020-03-18 22:47:06 +0100

    extract path to command line

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
-        println!("{:?}", args);
+    if args.len() > 1 {
+        for idx in 1..args.len() {
+            println!("{}", args[idx]);
+        }        
+    }else{
+        println!("Path missing");
     }
 }
