7427bdac54fd83daea77e56f3baa90a01480fb0c
Chonli <Chonli@users.noreply.github.com>
2020-03-18 23:44:22 +0100

    list file and dir of path

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use std::env;
+use std::{env, fs, io};
 
-fn main() {
+fn main() -> io::Result<()>  {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
         for idx in 1..args.len() {
-            println!("{}", args[idx]);
-        }        
-    }else{
+            let arg = args[idx].clone();
+            println!("{}", arg);
+            for entry in fs::read_dir(arg)? {
+                let entry = entry?;
+                let path = entry.path();
+                println!("{:?}", path);
+            }
+        }
+    } else {
         println!("Path missing");
     }
+    Ok(())
 }
