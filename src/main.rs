417ef3be8249a18790a3731e04ede44bbc18bf21
Chonli <Chonli@users.noreply.github.com>
2020-03-19 18:52:46 +0100

    Add count files lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 use std::path::Path;
 use std::{env, fs, io};
 
@@ -7,8 +10,7 @@ fn visit_dirs(dir: &Path, cb: &dyn Fn(&Path)) -> io::Result<()> {
             let entry = entry?;
             let file_name = entry.file_name().into_string().unwrap();
 
-            if file_name.chars().next() == Some('.') {
-                println!("hidden file {:?}", file_name);
+            if file_name.chars().nth(0) == Some('.') {
                 continue;
             }
             let path = entry.path();
@@ -22,16 +24,40 @@ fn visit_dirs(dir: &Path, cb: &dyn Fn(&Path)) -> io::Result<()> {
     Ok(())
 }
 
-fn file_callback(file: &Path) {
-    println!("{}", file.display());
+fn file_count_line_callback(file_path: &Path) {
+    let file = match File::open(&file_path) {
+        Err(_) => return,
+        Ok(file) => file,
+    };
+
+    let mut nb_line = 0;
+    let buf_reader = BufReader::new(file);
+    for line in buf_reader.lines() {
+        match line {
+            Err(_) => continue,
+            Ok(_) => nb_line += 1,
+        };
+    }
+    println!("file {:?} containt {} lines", file_path, nb_line);
+    unsafe {
+        NB_FILE_TOTAL += 1;
+        NB_LINE_TOTAL += nb_line;
+    }
 }
+
+static mut NB_FILE_TOTAL: u32 = 0;
+static mut NB_LINE_TOTAL: u32 = 0;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
         for idx in 1..args.len() {
-            println!("{}", args[idx]);
-            visit_dirs(args[idx].as_ref(), &file_callback);
+            visit_dirs(args[idx].as_ref(), &file_count_line_callback);
+        }
+        println!("=============================");
+
+        unsafe {
+            println!("{} files containt {} lines", NB_FILE_TOTAL, NB_LINE_TOTAL);
         }
     } else {
         println!("Path missing");
